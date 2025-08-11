f8e88b086ad64d776c6d7142c3aa0f5d431b82aa
obokhai <aibormoses1997@gmail.com>
2025-08-11 13:40:58 +0100

    .

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
     <div className='flex max-lg:flex-col items-center px-16 justify-center my-2'>
         <span className='flex flex-col max-lg:px-4 min-w-[400px] lg:border-r gap-y-3 lg:border-black pe-12'>
             <h3 className='capitalize lg:text-4xl text-2xl my-4 font-bold'>About hawkes Legal</h3>
-            <p className='leading-8 text-xs tracking-wide'>Hawkes Legal is a bespoke Nigerian law firm offering legal and business advisory services to a global clientele.  <br/>
+            <p className='leading-8 text-xs tracking-wide max-lg:max-w-xl'>Hawkes Legal is a bespoke Nigerian law firm offering legal and business advisory services to a global clientele.  <br/>
 The firm was founded with a vision of creating a value driven practice with global standards epitomizing efficiency and professionalism. <br/>Our clients entrust us with important legal decisions impacting on their business whilst we endeavor to be uptodate on the Law as it impacts our clients.    <br/>
 Our team is versatile, resourceful and always willing to attend to every detail of our clients’ concerns.</p>
         </span>
